37f55cbc82f3baab4e518df211c60b511141a1f7
Alexander Scott <xander@cadcoder.com>
2015-04-26 19:21:25 +1000

    Date added to player

--- schemas/Player.js
+++ schemas/Player.js
@@ -2,7 +2,8 @@ module.exports = function (mongoose) {
 
     ///User Schema
     var playerSchema = mongoose.Schema({
-        alias: {type: String, required: true, index: { unique: true }}
+        alias: {type: String, required: true, index: { unique: true }},
+        createdAt: {type: Date, default: Date.now }
     });
 
     return mongoose.model('Player', playerSchema);
