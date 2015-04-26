c657313ad4385ea9f0df4494d4d07795f20c86b1
Alexander Scott <xander@cadcoder.com>
2015-04-26 19:42:47 +1000

    Code Update

--- schemas/Code.js
+++ schemas/Code.js
@@ -2,7 +2,12 @@ module.exports = function (mongoose) {
 
     var codeSchema = mongoose.Schema({
         title: {type: String, required: true, index: { unique: true }},
-        description: String
+        description: String,
+        weight: Number,
+        location:{
+            latitude: Number,
+            longitude: Number
+        }
     });
 
     return mongoose.model('Code', codeSchema);
--- schemas/Player.js
+++ schemas/Player.js
@@ -3,7 +3,14 @@ module.exports = function (mongoose) {
     ///User Schema
     var playerSchema = mongoose.Schema({
         alias: {type: String, required: true, index: { unique: true }},
-        createdAt: {type: Date, default: Date.now }
+        createdAt: {type: Date, default: Date.now },
+        scans:[
+            {
+                event: {type: mongoose.Schema.Types.ObjectId, ref: 'Event'},
+                code: {type: mongoose.Schema.Types.ObjectId, ref: 'Code'},
+                scannedAt: {type: Date, default: Date.now}
+            }
+        ]
     });
 
     return mongoose.model('Player', playerSchema);
