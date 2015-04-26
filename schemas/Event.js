6560472dec844dfdfb37c23213e0fe990bf63e0b
Alexander Scott <xander@cadcoder.com>
2015-04-26 20:35:23 +1000

    Event Codes Array

--- schemas/Event.js
+++ schemas/Event.js
@@ -3,7 +3,12 @@ module.exports = function (mongoose) {
     var eventSchema = mongoose.Schema({
         title: {type: String, required: true, index: {unique: true}},
         description: String,
-        date: Date
+        date: Date,
+        codes: [
+            {
+                code: {type: mongoose.Schema.Types.ObjectId, ref: 'Code'}
+            }
+        ]
     });
 
     return mongoose.model('Event', eventSchema);
