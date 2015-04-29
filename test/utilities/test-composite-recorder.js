b4587e308c4eb66eb8e91727a94f06d910ba238a
Alexander Scott <xander@cadcoder.com>
2015-04-29 15:31:46 +1000

    Loggers

---
+++ test/utilities/test-composite-recorder.js
@@ -0,0 +1,53 @@
+var CompositeRecorder = require('../../lib/utilities/composite-recorder');
+var Recorder = require('../../lib/utilities/recorder');
+
+var cr = new CompositeRecorder();
+
+exports['testConstructor'] = function (test) {
+    test.strictEqual(cr.size(), 0);
+    test.done();
+};
+
+exports['testAddNonRecorder'] = function (test) {
+    cr.add({});
+    test.strictEqual(cr.size(), 0);
+    cr.add(null);
+    test.strictEqual(cr.size(), 0);
+    cr.add("");
+    test.strictEqual(cr.size(), 0);
+    test.done();
+};
+
+exports['testAddRecorder'] = function (test) {
+    cr.add(new Recorder());
+    test.strictEqual(cr.size(), 1);
+    cr.add(new Recorder());
+    cr.add(new Recorder());
+    test.strictEqual(cr.size(), 3);
+    test.done();
+};
+
+exports['testRemoveRecorder'] = function (test) {
+    var r = new Recorder();
+    cr.add(r);
+    test.strictEqual(cr.size(), 4);
+    cr.remove(r);
+    test.strictEqual(cr.size(), 3);
+    test.done();
+};
+
+exports['testRemoveNonRecorder'] = function (test) {
+    cr.remove({});
+    test.strictEqual(cr.size(), 3);
+    cr.remove(null);
+    test.strictEqual(cr.size(), 3);
+    cr.remove("");
+    test.strictEqual(cr.size(), 3);
+    test.done();
+};
+
+exports['testClear'] = function (test) {
+    cr.clear();
+    test.strictEqual(cr.size(), 0);
+    test.done();
+};
---
+++ test/utilities/test-strategy-logger.js
@@ -0,0 +1,44 @@
+var Recorder = require('../../lib/utilities/recorder');
+var Logger = require('../../lib/utilities/strategy-logger');
+var LoggerStrategy = require('../../lib/utilities/logger-strategy');
+var recorder = new Recorder();
+
+exports['testConstructor'] = function (test) {
+    var l = new Logger();
+    test.ok(LoggerStrategy.prototype.isPrototypeOf(l.strategy()));
+    test.done();
+};
+
+exports['testDefaultRecorder'] = function (test) {
+    var l = new Logger();
+    test.equals(l.getRecorder(), null);
+    test.done();
+};
+
+exports['testNullSetStrategy'] = function (test) {
+    var l = new Logger();
+    l.setStrategy(null);
+    test.ok(LoggerStrategy.prototype.isPrototypeOf(l.strategy()));
+    test.done();
+};
+
+exports['testEmptySetStrategy'] = function (test) {
+    var l = new Logger();
+    l.setStrategy();
+    test.ok(LoggerStrategy.prototype.isPrototypeOf(l.strategy()));
+    test.done();
+};
+
+exports['testObjectSetStrategy'] = function (test) {
+    var l = new Logger();
+    l.setStrategy({});
+    test.ok(LoggerStrategy.prototype.isPrototypeOf(l.strategy()));
+    test.done();
+};
+
+exports['testSetRecorder'] = function (test) {
+    var l = new Logger();
+    l.setRecorder(recorder);
+    test.ok(Recorder.prototype.isPrototypeOf(l.getRecorder()));
+    test.done();
+};
