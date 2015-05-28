4bad6ebee6d3f95facdbc861c7c67704623ce0ae
Alexander Scott <xander@cadcoder.com>
2015-05-28 22:17:27 +1000

    Codes Object Merging

--- routes/codes.js
+++ routes/codes.js
@@ -2,6 +2,7 @@ module.exports = function (server, models) {
     var Code = models['Code'];
     //Required for NotFoundError when a code does not exist
     var restify = require('restify');
+    var util = require('../lib/utilities/objects');
 
     function getAllCodes(request, response, next) {
         Code.find({}, function (err, codes) {
@@ -25,14 +26,16 @@ module.exports = function (server, models) {
 
     function updateCode(request, response, next) {
         Code.findOne({_id: request.body._id}, function (err, code) {
-            if (err) return next(new restify.NotFoundError("Unknown code"));
-            code.title = request.body.title;
-            code.description = request.body.description;
-            code.location = {latitude: request.body.latitude, longitude: request.body.longitude};
-            code.weight = request.body.weight;
+            if (err)
+                return next(new restify.NotFoundError("Unknown code"));
+
+
+            util.mergeObjects(code, request.body);
 
             code.save(function (err) {
-                if (err) throw err;
+                if (err){
+                    return next(new restify.InternalServerError(err));
+                }
                 response.send({code: code});
                 next();
             });
@@ -47,6 +50,8 @@ module.exports = function (server, models) {
      */
     function createCode(request, response, next) {
         var newCode = Code({title: request.body.title, description: request.body.description});
+        util.mergeObjects(newCode, request.body);
+
         newCode.save(function (err) {
             if (err) throw err;
             response.send({code:newCode});
